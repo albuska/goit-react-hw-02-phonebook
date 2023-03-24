829cc231c22bd48047667d80b8f683acbf708499
albuska <109683293+albuska@users.noreply.github.com>
2023-03-25 01:33:32 +0200

    good work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,72 @@
-import { Container } from './App.styled'; 
+import { Container, Title } from './App.styled';
 import { Component } from 'react';
-import { nanoid } from 'nanoid'; 
+import ContactForm from '../ContactForm';
+import ContactList from '../ContactList';
+import Filter from '../Filter';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-    state = {
-      contacts: [],
-      name: ''
-    }
+  formSubmitHandler = data => {
+    console.log(data);
+    const newContact = {
+      ...data,
+      id: nanoid(),
+    };
 
+    this.setState(prevState => {
+      return {
+        contacts: [newContact, ...prevState.contacts],
+      };
+    });
+  };
+
+  handleFilterInput = event => {
+    this.setState({
+      filter: event.currentTarget.value,
+    });
+  };
+
+  onDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
 
   render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    const filterContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
     return (
       <Container>
-        Hello baby
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <div>
+          <h2>Contacts</h2>
+          <Filter
+            filter={this.state.filter}
+            onChange={this.handleFilterInput}
+          />
+          <ContactList
+            contacts={filterContacts}
+            onDeleteContact={this.onDeleteContact}
+          />
+        </div>
       </Container>
     );
-  } 
-};
+  }
+}
 
 export default App;
--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,10 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 40px; 
-        color: #010101;
+  width: 500px;
+  padding: 40px 20px;
+  height: 100%;
+  margin: 0 auto;
+  align-items: center;
+  font-size: 20px;
+  background-image: linear-gradient(
+    90deg,
+    #020024 0%,
+    #090979 35%,
+    #00d4ff 100%
+  );
+  color: #ffffff;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  margin-bottom: 20px;
 `
